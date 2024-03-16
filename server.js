96fec31550d64eea6a8f0b0865849bb766439a55
Lebackrobot <lebackman18@gmail.com>
2024-03-15 21:13:18 -0300

    🌱 restart project

--- server.js
+++
@@ -1,5 +0,0 @@
-import app from './src/app.js'
-
-const port = process.env.PORT || 4000
-
-app.listen(port, () => console.log(`🌿 Server listen on port ${port}`))
--- src/routes/noauth/signin/signin-router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import signinController from '../../../controllers/signin/signin-controller.js'
-
-const signinRouter = express.Router()
-
-signinRouter
-    .post('/signin', signinController.signin)
-
-export default signinRouter
